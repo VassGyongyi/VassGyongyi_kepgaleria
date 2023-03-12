f2954a388f7230c9900aef334c0145c9940a9e97
VassGyongyi <s.v.gyongyi@gmail.com>
2023-03-12 18:15:29 +0100

    Button eseménykezelők

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 window.addEventListener("load", init);
 const KEPEK = [
-  "/kepek/DSC7025.jpg",
-  "/kepek/DSC7365.jpg",
-  "/kepek/DSC7371.jpg",
-  "/kepek/DSC7444.jpg",
-  "/kepek/DSC7515.jpg",
+  "kepek/DSC7025.jpg",
+  "kepek/DSC7365.jpg",
+  "kepek/DSC7371.jpg",
+  "kepek/DSC7444.jpg",
+  "kepek/DSC7515.jpg",
 ];
 function init() {
-  
-
   const ART = document.querySelector("article");
   //let kepEgy = document.innerHTML ="<div><img src='' alt='kép'><div>";
   for (let i = 0; i < KEPEK.length; i++) {
@@ -16,27 +14,55 @@ function init() {
   }
   const kepElem2 = document.querySelectorAll("article div img");
   for (let i = 0; i < kepElem2.length; i++) {
-    kepElem2[i].addEventListener("click", function(){egerEsemeny(i)});
+    kepElem2[i].addEventListener("click", function () {
+      egerEsemeny(i);
+    });
   }
-  GOMB = document.querySelectorAll("section button");
-  for (let i = 0; i < GOMB.length; i++) {
-  GOMB[i].addEventListener("click", function(){leptetes(i)});
-  }
+  GOMBBAL = document.getElementsByClassName("bal");
 
+  GOMBBAL[0].addEventListener("click", function () {
+    leptetesBal();
+  });
+  GOMBJOBB = document.getElementsByClassName("jobb");
+
+  GOMBJOBB[0].addEventListener("click", function () {
+    leptetesJobb();
+  });
 }
+
+
 function egerEsemeny(i) {
   const NAGYKEP = document.querySelectorAll("section div img");
   NAGYKEP[0].src = KEPEK[i];
 }
- function leptetes(i){
-  GOMBBAL = document.querySelector("bal");
-  GOMBJOBB = document.querySelector("jobb");
+function leptetesBal() {
   const NAGYKEP2 = document.querySelectorAll("section div img");
-  if(GOMBBAL){
-    NAGYKEP2[0].src = KEPEK[i-1];
-  }
-  if(GOMBJOBB){
-    NAGYKEP2[0].src = KEPEK[i+1];
-  }
-  
- }
+  for (let i = 0; i < KEPEK.length; i++)
+    if (i > 0) {
+      if (
+        KEPEK[i] ==
+        NAGYKEP2[0].src.substring(
+          NAGYKEP2[0].src.length - 17,
+          NAGYKEP2[0].src.length
+        )
+      ) {
+        NAGYKEP2[0].src = KEPEK[--i];
+      }
+    }
+}
+function leptetesJobb() {
+  const NAGYKEP3 = document.querySelectorAll("section div img");
+  for (let i = 0; i < KEPEK.length; i++)
+    if (i < KEPEK.length-1) {
+      console.log(i);
+      if (
+        KEPEK[i] ==
+        NAGYKEP3[0].src.substring(
+          NAGYKEP3[0].src.length - 17,
+          NAGYKEP3[0].src.length
+        )
+      ) {
+        NAGYKEP3[0].src = KEPEK[++i];
+      }
+    }
+}
